32ee8373f266a8590c662d87d5cf4ae3a06667c1
NicoleWang <434707665@qq.com>
2019-05-29 13:39:36 +0800

    modify show_single

--- show_single.sh
+++ show_single.sh
@@ -1,10 +1,8 @@
-DEPLOY_PROTO='./nets/tracker.prototxt'		 
-CAFFE_MODEL='./nets/tracker.pt'		
+MODEL_PATH='./nets/tracker.pt'		
 TEST_DATA_PATH='./data/vot14/'		
 #TEST_DATA_PATH='./data/multi'		
 
 python -m goturn_single.test.show_tracker_vot \
-	--p $DEPLOY_PROTO \
-	--m $CAFFE_MODEL \
+	--m $MODEL_PATH \
 	--i $TEST_DATA_PATH \
 	--g 0
