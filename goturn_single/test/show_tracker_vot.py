4d8c6f16bfa189f58deee33566166ceb23fc8342
NicoleWang <434707665@qq.com>
2019-05-29 14:03:13 +0800

    using pytorch regressor

--- goturn_single/test/show_tracker_vot.py
+++ goturn_single/test/show_tracker_vot.py
@@ -4,28 +4,26 @@
 # Description: Test file for showing the tracker output
 import sys
 import argparse
-#import setproctitle
 from ..logger.logger import setup_logger
-#from ..network.regressor import regressor
 from ..network.regressor import AlexNet
 from ..loader.loader_vot import loader_vot
 #from ..loader.loader_vot import loader_bit
 from ..tracker.tracker import tracker
 from ..tracker.tracker_manager import tracker_manager
 import torch 
-#setproctitle.setproctitle('SHOW_TRACKER_VOT')
 logger = setup_logger(logfile=None)
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True, help="Path to the prototxt")
+#ap.add_argument("-p", "--prototxt", required=True, help="Path to the prototxt")
 ap.add_argument("-m", "--model", required=True, help="Path to the model")
 ap.add_argument("-v", "--input", required=True, help="Path to the vot directory")
 ap.add_argument("-g", "--gpuID", required=True, help="gpu to use")
 args = vars(ap.parse_args())
 
 do_train = False
-#objRegressor = regressor(args['prototxt'], args['model'], args['gpuID'], 1, do_train, logger)
+
 objRegressor = AlexNet()
+#load pytorch model from caffe model
 alex_dict = objRegressor.state_dict()
 pre = torch.load(args['model'])
 new = {k.replace('-','_'):v for k, v in pre.items() if '_p' not in k}
